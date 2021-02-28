50c3ac2d234af96b7e6b8b153d39df5bcbd7c2dd
anastminsk <anast-minsk@yandex.ru>
2021-02-28 16:40:59 +0300

    add styled-components functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from "react";
-import HelloReact from "./components/HelloReact";
-import "./App.less";
+import React from 'react';
+import styled from 'styled-components';
+
+const Root = styled.div`
+	text-align: center;
+`;
+
+const HelloReact = styled.h1`
+	color: #f65261;
+	background-color: #232323;
+`;
 
 function App() {
 	return (
-		<div className="app">
-			<HelloReact />
-		</div>
+		<Root>
+			<HelloReact>Hello React!</HelloReact>
+		</Root>
 	);
 }
 
