c4a564e0863483a6155b1c07d5c8def037635968
anastminsk <anast-minsk@yandex.ru>
2021-04-05 00:11:56 +0300

    add formik, modify modal add/edit content

--- src/components/ModalAddEditContent/index.jsx
+++ src/components/ModalAddEditContent/index.jsx
@@ -1,4 +1,6 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import {
 	InputLabel,
 	MenuItem,
@@ -16,6 +18,37 @@ import {
 } from './styled.modal-add-edit-content';
 import { GENRES_OPTIONS } from '../MainContent/model';
 
+const initialFormData = {
+	title: '',
+	genres: [],
+	overview: '',
+	poster_path: '',
+	runtime: 0,
+	release_date: '',
+	tagline: '',
+	vote_average: 0,
+};
+
+const validationSchema = yup.object({
+	title: yup.string().required('Title is a required field'),
+	genres: yup
+		.array()
+		.min(1, 'Minimum one element is required')
+		.required('Genres is a required field'),
+	overview: yup.string().required('Overview is a required field'),
+	poster_path: yup
+		.string()
+		.url('Movie URL must be valid')
+		.required('Movie URL is a required field'),
+	runtime: yup.number().min(0).required('Runtime is a required field'),
+	release_date: yup.string().required('Release date is a required field'),
+	tagline: yup.string().required('Tagline is a required field'),
+	vote_average: yup
+		.number()
+		.min(0)
+		.required('Vote average is a required field'),
+});
+
 const ModalAddEditContent = ({
 	closeModal,
 	onModalSubmit,
@@ -23,54 +56,21 @@ const ModalAddEditContent = ({
 	formData,
 }) => {
 	const movieID = editableMode ? formData?.id : '';
-	const [genres, setGenres] = useState(editableMode ? formData?.genres : []);
-	const [title, setTitle] = useState(editableMode ? formData?.title : '');
-	const [overview, setOverview] = useState(
-		editableMode ? formData?.overview : ''
-	);
-	const [poster_path, setMovieUrl] = useState(
-		editableMode ? formData?.poster_path : ''
-	);
-	const [runtime, setRuntime] = useState(
-		editableMode ? formData?.runtime : ''
-	);
-	const [release_date, setReleaseDate] = useState(
-		editableMode ? formData?.release_date : ''
-	);
-	const [tagline, setTagline] = useState(
-		editableMode ? formData?.tagline : ''
-	);
-	const [vote_average, setVoteAverage] = useState(
-		editableMode ? formData?.vote_average : ''
-	);
-
-	const handleChangeTitle = (event) => {
-		setTitle(event.target.value);
-	};
-	const handleChangeGenres = (event) => {
-		setGenres(event.target.value);
-	};
-	const handleChangeOverview = (event) => {
-		setOverview(event.target.value);
-	};
-	const handleChangeMovieUrl = (event) => {
-		setMovieUrl(event.target.value);
-	};
-	const handleChangeRuntime = (event) => {
-		setRuntime(Number(event.target.value));
-	};
-	const handleChangeReleaseDate = (event) => {
-		setReleaseDate(event.target.value);
-	};
-	const handleChangeTagline = (event) => {
-		setTagline(event.target.value);
-	};
-	const handleChangeVoteAverage = (event) => {
-		setVoteAverage(Number(event.target.value));
-	};
+	const formik = useFormik({
+		initialValues: editableMode ? formData : initialFormData,
+		validationSchema: validationSchema,
+		onSubmit: (values) => {
+			onModalSubmit({
+				id: formData.id,
+				...values,
+				runtime: +values.runtime,
+				vote_average: +values.vote_average,
+			});
+		},
+	});
 
 	return (
-		<form noValidate autoComplete="off">
+		<form onSubmit={formik.handleSubmit}>
 			<Fragment>
 				<StyledTextField
 					style={{ display: editableMode ? 'block' : 'none' }}
@@ -82,41 +82,46 @@ const ModalAddEditContent = ({
 				<StyledTextField
 					label="Title"
 					name="title"
-					value={title}
+					value={formik.values.title}
 					fullWidth
-					onChange={handleChangeTitle}
+					onChange={formik.handleChange}
 				/>
 				<StyledTextField
 					type="date"
 					label="Release date"
-					name="releaseDate"
-					value={release_date}
+					name="release_date"
+					value={formik.values.release_date}
 					fullWidth
 					InputLabelProps={{
 						shrink: true,
 					}}
-					onChange={handleChangeReleaseDate}
+					onChange={formik.handleChange}
 				/>
 				<StyledTextField
-					label="Movie URL"
-					name="movieUrl"
-					value={poster_path}
+					label="Poster path"
+					name="poster_path"
+					value={formik.values.poster_path}
 					fullWidth
-					onChange={handleChangeMovieUrl}
+					onChange={formik.handleChange}
 				/>
 				<StyledFormControl fullWidth>
-					<InputLabel id="genre-label">Genres</InputLabel>
+					<InputLabel id="genres-label">Genres</InputLabel>
 					<Select
+						name="genres"
 						multiple
-						labelId="genre-label"
-						value={genres}
-						onChange={handleChangeGenres}
-						input={<Input />}
+						labelId="genres-label"
+						value={formik.values.genres}
+						onChange={formik.handleChange}
+						input={<Input name="genres" />}
 						renderValue={(selected) => selected.join(', ')}
 					>
 						{GENRES_OPTIONS.map((item) => (
 							<MenuItem key={item} value={item}>
-								<Checkbox checked={genres.indexOf(item) > -1} />
+								<Checkbox
+									checked={
+										formik.values.genres.indexOf(item) > -1
+									}
+								/>
 								<ListItemText primary={item} />
 							</MenuItem>
 						))}
@@ -125,52 +130,37 @@ const ModalAddEditContent = ({
 				<StyledTextField
 					label="Overview"
 					name="overview"
-					value={overview}
+					value={formik.values.overview}
 					fullWidth
-					onChange={handleChangeOverview}
+					onChange={formik.handleChange}
 				/>
 				<StyledTextField
 					label="Runtime"
 					name="runtime"
-					value={runtime}
+					value={formik.values.runtime}
 					fullWidth
-					onChange={handleChangeRuntime}
+					onChange={formik.handleChange}
 				/>
 				<StyledTextField
 					label="Tagline"
 					name="tagline"
-					value={tagline}
+					value={formik.values.tagline}
 					fullWidth
-					onChange={handleChangeTagline}
+					onChange={formik.handleChange}
 				/>
 				<StyledTextField
-					label="VoteAverage"
-					name="voteAverage"
-					value={vote_average}
+					label="Vote average"
+					name="vote_average"
+					value={formik.values.vote_average}
 					fullWidth
-					onChange={handleChangeVoteAverage}
+					onChange={formik.handleChange}
 				/>
 			</Fragment>
 			<ButtonsWrapper>
 				<ResetButton variant="outlined" onClick={closeModal}>
 					Reset
 				</ResetButton>
-				<SubmitButton
-					variant="outlined"
-					onClick={() =>
-						onModalSubmit({
-							...formData,
-							genres,
-							title,
-							overview,
-							poster_path,
-							runtime,
-							release_date,
-							tagline,
-							vote_average,
-						})
-					}
-				>
+				<SubmitButton variant="outlined" type="submit">
 					Submit
 				</SubmitButton>
 			</ButtonsWrapper>
