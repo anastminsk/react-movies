7a87a48c821dad0d1a485349feb1779e8752fdb2
anastminsk <anast-minsk@yandex.ru>
2021-04-12 14:23:20 +0300

    modify actions and reducers

--- src/redux/reducers/movies.jsx
+++ src/redux/reducers/movies.jsx
@@ -2,54 +2,41 @@ import * as ACTIONS from '../actions/actionTypes';
 
 const initialState = {
 	movies: [],
-	loading: false,
 	error: null,
 	currentMovie: null,
 	filter: 'All',
 	sorting: 'release_date',
+	search: null,
 };
 
 const movies = (state = initialState, action) => {
 	switch (action.type) {
 		case ACTIONS.FETCH_MOVIES_START:
-			return {
-				...state,
-				loading: true,
-			};
+			return state;
 		case ACTIONS.FETCH_MOVIES_SUCCESS:
 			return {
 				...state,
-				loading: false,
 				movies: action.movies,
 			};
 		case ACTIONS.FETCH_MOVIES_ERROR:
 			return {
 				...state,
-				loading: false,
 				error: action.error,
 			};
 		case ACTIONS.FETCH_MOVIE_START:
-			return {
-				...state,
-				loading: true,
-			};
+			return state;
 		case ACTIONS.FETCH_MOVIE_SUCCESS:
 			return {
 				...state,
-				loading: false,
 				currentMovie: action.currentMovie,
 			};
 		case ACTIONS.FETCH_MOVIE_ERROR:
 			return {
 				...state,
-				loading: false,
 				error: action.error,
 			};
 		case ACTIONS.CREATE_MOVIE_START:
-			return {
-				...state,
-				loading: true,
-			};
+			return state;
 		case ACTIONS.CREATE_MOVIE_SUCCESS:
 			return {
 				...state,
@@ -58,14 +45,10 @@ const movies = (state = initialState, action) => {
 		case ACTIONS.CREATE_MOVIE_ERROR:
 			return {
 				...state,
-				loading: false,
 				error: action.error,
 			};
 		case ACTIONS.EDIT_MOVIE_START:
-			return {
-				...state,
-				loading: true,
-			};
+			return state;
 		case ACTIONS.EDIT_MOVIE_SUCCESS:
 			return {
 				...state,
@@ -76,14 +59,10 @@ const movies = (state = initialState, action) => {
 		case ACTIONS.EDIT_MOVIE_ERROR:
 			return {
 				...state,
-				loading: false,
 				error: action.error,
 			};
 		case ACTIONS.DELETE_MOVIE_START:
-			return {
-				...state,
-				loading: true,
-			};
+			return state;
 		case ACTIONS.DELETE_MOVIE_SUCCESS:
 			return {
 				...state,
@@ -92,14 +71,10 @@ const movies = (state = initialState, action) => {
 		case ACTIONS.DELETE_MOVIE_ERROR:
 			return {
 				...state,
-				loading: false,
 				error: action.error,
 			};
 		case ACTIONS.FILTER_MOVIES_START:
-			return {
-				...state,
-				loading: true,
-			};
+			return state;
 		case ACTIONS.FILTER_MOVIES_SUCCESS:
 			return {
 				...state,
@@ -109,14 +84,10 @@ const movies = (state = initialState, action) => {
 		case ACTIONS.FILTER_MOVIES_ERROR:
 			return {
 				...state,
-				loading: false,
 				error: action.error,
 			};
 		case ACTIONS.SORT_MOVIES_START:
-			return {
-				...state,
-				loading: true,
-			};
+			return state;
 		case ACTIONS.SORT_MOVIES_SUCCESS:
 			return {
 				...state,
@@ -126,7 +97,19 @@ const movies = (state = initialState, action) => {
 		case ACTIONS.SORT_MOVIES_ERROR:
 			return {
 				...state,
-				loading: false,
+				error: action.error,
+			};
+		case ACTIONS.SEARCH_MOVIE_START:
+			return state;
+		case ACTIONS.SEARCH_MOVIE_SUCCESS:
+			return {
+				...state,
+				movies: action.movies,
+				search: action.search,
+			};
+		case ACTIONS.SEARCH_MOVIE_ERROR:
+			return {
+				...state,
 				error: action.error,
 			};
 		default:
